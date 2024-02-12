10fb911a5fef38682f3862ef8cc9efe9de8e67ed
welldonestack <154399320+welldonestack@users.noreply.github.com>
2024-02-12 16:13:07 +1100

    Add files via upload
    change due to class reorganise

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
 let record = false;
-let canvas;
+let poster,
+  posterW = 596,
+  posterH = 842;
 // let path;
 
 P5Capture.setDefaultOptions({
@@ -24,11 +26,15 @@ function preload() {
   for (let i = 0; i < 5; i++) {
     singersPic[i] = loadImage("img/concert" + i + ".png");
   }
+  iconPic = [];
+  for (let i = 0; i < 3; i++) {
+    iconPic[i] = loadImage("img/icon" + i + ".png");
+  }
 }
 
 function setup() {
   createCanvas(900, 900);
-  canvas = new Canvas(596, 842);
+  poster = new Poster(posterW, posterH);
   // path = new Path(posterW);
   // noLoop();
 }
@@ -38,13 +44,14 @@ function draw() {
     if (frameCount === 1) {
       const capture = P5Capture.getInstance();
       capture.start({
-        duration: 240,
+        duration: 1000,
       });
     }
   }
   background(0);
-  canvas.show();
+  poster.show();
   // path.show();
+  hover();
 }
 
 function keyPressed() {
@@ -56,27 +63,82 @@ function keyPressed() {
 
 function mouseDragged() {
   let offsetMouse = createVector(
-    (width - canvas.posterW) / 2,
-    (height - canvas.posterH) / 2
+    (width - posterW) / 2,
+    (height - posterH) / 2
   );
   let mousePos = createVector(mouseX, mouseY);
   mousePos.sub(offsetMouse);
 
-  for (let i = 0; i < canvas.emoji.length; i++) {
-    let offsetEmoji = [];
-    let offsetEmojiPos = [];
-    offsetEmoji[i] = createVector(canvas.emoji[i].w / 2, canvas.emoji[i].h / 2);
-    offsetEmojiPos[i] = canvas.emoji[i].position.copy();
-    offsetEmojiPos[i].add(offsetEmoji[i]); // offset to center of the emoji
+  let distance = [];
+ for (let i = 0; i < poster.emoji.emoji.length; i++) {
+    distance[i] = p5.Vector.dist(poster.emoji.emoji[i].position, mousePos);
+  }
 
-    let distance = [];
-    distance[i] = p5.Vector.dist(offsetEmojiPos[i], mousePos);
-    if (distance[i] < canvas.emoji[i].h / 2) {
-      canvas.emoji[i].position.x = mousePos.x - canvas.emoji[i].w / 2;
-      canvas.emoji[i].position.y = mousePos.y - canvas.emoji[i].h / 2;
-      //replay motions
-      //canvas.textParticlesInLiquid.initialize(53, 80, 660, 0.5, 1, 0.5);
-      canvas.resetSingerImage();
+  if (distance[0] < poster.emoji.emoji[0].w / 2) {
+    // poster.emoji[0].alphaValue = 255;
+
+    let dragLimitX = poster.emoji.emojiTarget[0].x - poster.emoji.emoji[0].w / 2;
+    poster.emoji.emoji[0].position.x = map(
+      mousePos.x,
+      0,
+      posterW,
+      dragLimitX,
+      poster.emoji.emojiTarget[0].x
+    );
+    poster.emoji.emoji[0].position.y = poster.emoji.emojiTarget[0].y;
+    //poster.textParticlesInLiquid.initialize(53, 80, 660, 0.5, 1, 0.5);
+    poster.singer.initialize();
+  }
+
+  if (distance[1] < poster.emoji.emoji[1].w / 2) {
+    // poster.emoji[1].alphaValue = 255;
+
+    let dragLimitY = poster.emoji.emojiTarget[1].y + poster.emoji.emoji[1].h / 15;
+    poster.emoji.emoji[1].position.x = poster.emoji.emojiTarget[1].x;
+    poster.emoji.emoji[1].position.y = map(
+      mousePos.y,
+      0,
+      posterH + poster.emoji.emoji[1].h * 1,
+      poster.emoji.emojiTarget[1].y,
+      dragLimitY
+    );
+    poster.singer.initialize();
+    poster.singerList.initialize();
+  }
+}
+
+function hover() {
+  let offsetMouse = createVector((width - posterW) / 2, (height - posterH) / 2);
+  let mousePos = createVector(mouseX, mouseY);
+  mousePos.sub(offsetMouse);
+  let distance = [];
+  for (let i = 0; i < poster.emoji.emoji.length; i++) {
+    distance[i] = p5.Vector.dist(poster.emoji.emoji[i].position, mousePos);
+    if (distance[i] < poster.emoji.emoji[i].h / 2) {
+      poster.emoji.emoji[i].alphaValue = 255;
+      poster.icon.icon[i].alphaValue = 70;
+    } else {
+      poster.emoji.emoji[i].alphaValue = 70;
+      poster.icon.icon[i].alphaValue = 0;
     }
+  }
+}
+
+function mousePressed() {
+  let offsetMouse = createVector(
+    (width - posterW) / 2,
+    (height - posterH) / 2
+  );
+  let mousePos = createVector(mouseX, mouseY);
+  mousePos.sub(offsetMouse);
+
+  let distance = [];
+  for (let i = 0; i < poster.emoji.emoji.length; i++) {
+    distance[i] = p5.Vector.dist(poster.emoji.emoji[i].position, mousePos);
+  }
+
+  if (distance[2] < poster.emoji.emoji[2].h / 2) {
+    poster.emoji.emoji[2].alphaValue = 255;
+    poster.toggle = !poster.toggle;
   }
 }
